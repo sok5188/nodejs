7e052af93729bab9bdbef73c2a46a0e9c68b4248
sok5188 <sok5188@gmail.com>
2022-02-14 21:56:30 +0900

    2022-02-14

--- study/main.js
+++ study/main.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-
+let qs = require("querystring");
 function templateHTML(title, list, body) {
   return `
             <!doctype html>
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
            <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+              <a href="/create">create</a>
              ${body}
             </body>
             </html>
@@ -65,6 +66,42 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", function (err, filelist) {
+      var title = "CREATE";
+
+      data = "Hello, Node.js";
+      var list = templateLIST(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/create_process" method="post">
+        <p><input ="text" name="title" placeholder="input title"></p>
+        <p><textarea name="description" placeholder ="input description"></textarea></p>
+        <p>
+         <input type="submit" />
+         </p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    let body = "";
+    request.on("data", function (data) {
+      body += data; //body에 콜백시마다 data추가
+    });
+    request.on("end", function () {
+      let post = qs.parse(body);
+      let title = post.title;
+      let description = post.description;
+      fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end("ok");
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("Not Found");
