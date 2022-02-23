c49b1d758b11bdcb1dc373bdc8a7852fa345351c
sok5188 <sok5188@gmail.com>
2022-02-23 21:02:04 +0900

    2022-02-23

--- project/main.js
+++ project/main.js
@@ -5,41 +5,43 @@ let dirname = args[2];
 // modified process
 // 마지막 언더바 뒤에 E가 추가된다고 가정.
 //refactoring not yet.
+function is_modified(filename, list) {
+  let tmp_file = filename.split("_");
+  let tmp_size = tmp_file.length;
+  let target = tmp_file[tmp_size - 1].charAt(0);
+  let i = 0;
+  if (target == "E") {
+    //maybe modified
+    tmp_file[tmp_size - 1] = tmp_file[tmp_size - 1].replace("E", "");
+    let sub = tmp_file[0];
+    if (tmp_size > 1) {
+      for (let j = 1; j < tmp_size; j++) {
+        sub += "_" + tmp_file[j];
+      }
+    }
+    while (i < list.length) {
+      let compare_sub = list[i];
+      i += 1;
+      if (compare_sub == sub) {
+        //modified!
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function movefile(dirname, newdir, filename) {
+  if (!fs.existsSync(`${dirname}/${newdir}`)) {
+    fs.mkdirSync(`${dirname}/${newdir}`);
+  }
+  fs.renameSync(`${dirname}/${filename}`, `${dirname}/${newdir}/${filename}`);
+}
 
 fs.readdir(dirname, function (err, list) {
   if (err) console.log("cannot read directory");
-  function is_modified(filename) {
-    let tmp_file = filename.split("_");
 
-    let tmp_size = tmp_file.length;
-
-    let target = tmp_file[tmp_size - 1].charAt(0);
-    let i = 0;
-    if (target == "E") {
-      //maybe modified
-      tmp_file[tmp_size - 1] = tmp_file[tmp_size - 1].replace("E", "");
-
-      let sub = tmp_file[0];
-
-      if (tmp_size > 1) {
-        for (let j = 1; j < tmp_size; j++) {
-          sub += "_" + tmp_file[j];
-        }
-      }
-
-      while (i < list.length) {
-        let compare_sub = list[i];
-        i += 1;
-
-        if (compare_sub == sub) {
-          //modified!
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
   let i = 0;
   while (i < list.length) {
     let filename = list[i];
@@ -49,47 +51,18 @@ fs.readdir(dirname, function (err, list) {
     let extension = target[1];
     let ival = name.indexOf("_E");
     if (ival != -1) {
-      if (is_modified(filename)) {
-        let newdir = "edited";
-        if (!fs.existsSync(`${dirname}/${newdir}`)) {
-          fs.mkdirSync(`${dirname}/${newdir}`);
-        }
-        fs.renameSync(
-          `${dirname}/${filename}`,
-          `${dirname}/${newdir}/${filename}`
-        );
+      if (is_modified(filename, list)) {
+        movefile(dirname, "edited", filename);
         continue;
       }
     }
 
     if (extension == "mp4" || extension == "mov") {
-      let newdir = "video";
-      if (!fs.existsSync(`${dirname}/${newdir}`)) {
-        fs.mkdirSync(`${dirname}/${newdir}`);
-      }
-
-      fs.renameSync(
-        `${dirname}/${filename}`,
-        `${dirname}/${newdir}/${filename}`
-      );
+      movefile(dirname, "video", filename);
     } else if (extension == "jpg") {
-      let newdir = "photo";
-      if (!fs.existsSync(`${dirname}/${newdir}`)) {
-        fs.mkdirSync(`${dirname}/${newdir}`);
-      }
-      fs.renameSync(
-        `${dirname}/${filename}`,
-        `${dirname}/${newdir}/${filename}`
-      );
+      movefile(dirname, "photo", filename);
     } else if (extension == "png" || extension == "aae") {
-      let newdir = "captured";
-      if (!fs.existsSync(`${dirname}/${newdir}`)) {
-        fs.mkdirSync(`${dirname}/${newdir}`);
-      }
-      fs.renameSync(
-        `${dirname}/${filename}`,
-        `${dirname}/${newdir}/${filename}`
-      );
+      movefile(dirname, "captured", filename);
     }
   }
 });
